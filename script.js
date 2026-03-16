07a1833c208a42f2b9bcce2bf68fd07748fd9f4b
kledilsonvieira <kledilsonvieira1@gmail.com>
2026-03-16 17:44:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function falarIA(){
+
+let pergunta = prompt("Pergunte algo sobre nossos móveis:")
+
+if(pergunta){
+
+alert("IA: Obrigado pela pergunta! Nossa equipe vai entrar em contato.")
+
+}
+
+}
